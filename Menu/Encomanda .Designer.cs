b2a0bff3e79415f2363341d73a3b3639a0bcf7b0
NICOLAS RANGEL DE GOES COUTINHO <nicolas.rgcoutinho@senacsp.edu.br>
2026-03-30 16:56:10 -0300

    foi

---
+++ Menu/Encomanda .Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Menu
+{
+    partial class Encomanda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Encomanda));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            button1 = new Button();
+            txtMenu = new Button();
+            panel3 = new Panel();
+            button3 = new Button();
+            button4 = new Button();
+            button2 = new Button();
+            label4 = new Label();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(192, 0, 0);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(199, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1416, 115);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(64, 64, 64);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(txtMenu);
+            panel2.Controls.Add(panel3);
+            panel2.Location = new Point(2, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(200, 780);
+            panel2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(38, 25);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(117, 90);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Impact", 48F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlLight;
+            label1.Location = new Point(371, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(369, 80);
+            label1.TabIndex = 2;
+            label1.Text = "Encomendas";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 189);
+            button1.Name = "button1";
+            button1.Size = new Size(197, 66);
+            button1.TabIndex = 4;
+            button1.Text = "Estoque";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // txtMenu
+            // 
+            txtMenu.Location = new Point(0, 352);
+            txtMenu.Name = "txtMenu";
+            txtMenu.Size = new Size(197, 66);
+            txtMenu.TabIndex = 5;
+            txtMenu.Text = "Menu";
+            txtMenu.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(192, 0, 0);
+            panel3.Location = new Point(55, 112);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(86, 581);
+            panel3.TabIndex = 7;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(318, 157);
+            button3.Name = "button3";
+            button3.Size = new Size(207, 49);
+            button3.TabIndex = 6;
+            button3.Text = "Pizza de calabresa";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(318, 371);
+            button4.Name = "button4";
+            button4.Size = new Size(207, 49);
+            button4.TabIndex = 7;
+            button4.Text = "Pastel de calabresa";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(318, 551);
+            button2.Name = "button2";
+            button2.Size = new Size(207, 49);
+            button2.TabIndex = 10;
+            button2.Text = "Coca 1L";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(230, 388);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 15);
+            label4.TabIndex = 11;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(629, 551);
+            button5.Name = "button5";
+            button5.Size = new Size(207, 49);
+            button5.TabIndex = 12;
+            button5.Text = "Sprite 1L";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(629, 371);
+            button6.Name = "button6";
+            button6.Size = new Size(207, 49);
+            button6.TabIndex = 13;
+            button6.Text = "Pizza de frango c/ catutipy";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(629, 157);
+            button7.Name = "button7";
+            button7.Size = new Size(207, 49);
+            button7.TabIndex = 14;
+            button7.Text = "Pizza de frango c/ catupiry";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(923, 157);
+            button8.Name = "button8";
+            button8.Size = new Size(207, 49);
+            button8.TabIndex = 15;
+            button8.Text = "Pizza de calabresa";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(923, 551);
+            button9.Name = "button9";
+            button9.Size = new Size(207, 49);
+            button9.TabIndex = 16;
+            button9.Text = "Pizza de calabresa";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(933, 371);
+            button10.Name = "button10";
+            button10.Size = new Size(207, 49);
+            button10.TabIndex = 17;
+            button10.Text = "Pizza de calabresa";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // Encomanda
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SeaShell;
+            ClientSize = new Size(1444, 722);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(panel1);
+            Controls.Add(panel2);
+            Name = "Encomanda";
+            Text = "Encomanda";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Button txtMenu;
+        private Button button1;
+        private Panel panel3;
+        private Button button3;
+        private Button button4;
+        private Button button2;
+        private Label label4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+    }
+}
