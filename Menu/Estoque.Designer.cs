efd869aa4c02ff786cc316ae357e3e623e502d63
ALISON SERAFIM DE SOUZA <alison.ssouza1@senacsp.edu.br>
2026-03-30 17:02:45 -0300

    att

---
+++ Menu/Estoque.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Menu
+{
+    partial class Estoque
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Estoque));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            dataGridView1 = new DataGridView();
+            pictureBox3 = new PictureBox();
+            ID = new DataGridViewTextBoxColumn();
+            Produto = new DataGridViewTextBoxColumn();
+            Quant = new DataGridViewTextBoxColumn();
+            Validade = new DataGridViewTextBoxColumn();
+            EmFalta = new DataGridViewTextBoxColumn();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(192, 0, 0);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(-47, -7);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1399, 132);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(64, 64, 64);
+            panel2.Controls.Add(pictureBox3);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(button2);
+            panel2.Location = new Point(-6, -4);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(200, 783);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Impact", 48F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(680, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(243, 80);
+            label1.TabIndex = 0;
+            label1.Text = "Estoque";
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Times New Roman", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(3, 212);
+            button1.Name = "button1";
+            button1.Size = new Size(197, 66);
+            button1.TabIndex = 4;
+            button1.Text = "        Encomendas";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Times New Roman", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(3, 408);
+            button2.Name = "button2";
+            button2.Size = new Size(197, 66);
+            button2.TabIndex = 2;
+            button2.Text = "Voltar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(18, 221);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(42, 48);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(18, 417);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(42, 48);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { ID, Produto, Quant, Validade, EmFalta });
+            dataGridView1.Location = new Point(414, 286);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(689, 424);
+            dataGridView1.TabIndex = 2;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(18, 7);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(169, 122);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // ID
+            // 
+            ID.HeaderText = "ID";
+            ID.Name = "ID";
+            ID.Width = 43;
+            // 
+            // Produto
+            // 
+            Produto.HeaderText = "Produto";
+            Produto.Name = "Produto";
+            // 
+            // Quant
+            // 
+            Quant.HeaderText = "Quant";
+            Quant.Name = "Quant";
+            // 
+            // Validade
+            // 
+            Validade.HeaderText = "Validade";
+            Validade.Name = "Validade";
+            // 
+            // EmFalta
+            // 
+            EmFalta.HeaderText = "EmFalta";
+            EmFalta.Name = "EmFalta";
+            // 
+            // Estoque
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SeaShell;
+            ClientSize = new Size(1347, 776);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Estoque";
+            Text = "Estoque";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private DataGridView dataGridView1;
+        private PictureBox pictureBox3;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn Produto;
+        private DataGridViewTextBoxColumn Quant;
+        private DataGridViewTextBoxColumn Validade;
+        private DataGridViewTextBoxColumn EmFalta;
+    }
+}
