e594fef8fb2c37f821df80362e131a4b76ea33f8
NICOLAS RANGEL DE GOES COUTINHO <nicolas.rgcoutinho@senacsp.edu.br>
2026-03-31 14:29:24 -0300

    nicol

--- Menu/Encomanda .Designer.cs
+++ Menu/Encomanda .Designer.cs
@@ -30,10 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Encomanda));
             panel1 = new Panel();
-            panel2 = new Panel();
-            pictureBox2 = new PictureBox();
             label1 = new Label();
+            panel2 = new Panel();
             button1 = new Button();
+            pictureBox2 = new PictureBox();
             txtMenu = new Button();
             panel3 = new Panel();
             button3 = new Button();
@@ -55,32 +55,10 @@
             // 
             panel1.BackColor = Color.FromArgb(192, 0, 0);
             panel1.Controls.Add(label1);
-            panel1.Location = new Point(199, 2);
+            panel1.Location = new Point(202, -4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1416, 115);
+            panel1.Size = new Size(1415, 121);
             panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.FromArgb(64, 64, 64);
-            panel2.Controls.Add(button1);
-            panel2.Controls.Add(pictureBox2);
-            panel2.Controls.Add(txtMenu);
-            panel2.Controls.Add(panel3);
-            panel2.Location = new Point(2, 2);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(200, 780);
-            panel2.TabIndex = 1;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(38, 25);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(117, 90);
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox2.TabIndex = 3;
-            pictureBox2.TabStop = false;
             // 
             // label1
             // 
@@ -94,58 +72,97 @@
             label1.Text = "Encomendas";
             label1.Click += label1_Click;
             // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(64, 64, 64);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(txtMenu);
+            panel2.Controls.Add(panel3);
+            panel2.Location = new Point(2, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(200, 783);
+            panel2.TabIndex = 1;
+            // 
             // button1
             // 
+            button1.BackColor = Color.FromArgb(64, 64, 64);
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
             button1.Location = new Point(0, 189);
             button1.Name = "button1";
             button1.Size = new Size(197, 66);
             button1.TabIndex = 4;
             button1.Text = "Estoque";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(38, 25);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(117, 90);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
             // 
             // txtMenu
             // 
+            txtMenu.BackColor = Color.FromArgb(64, 64, 64);
+            txtMenu.FlatStyle = FlatStyle.Popup;
+            txtMenu.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtMenu.ForeColor = Color.White;
             txtMenu.Location = new Point(0, 352);
             txtMenu.Name = "txtMenu";
             txtMenu.Size = new Size(197, 66);
             txtMenu.TabIndex = 5;
             txtMenu.Text = "Menu";
-            txtMenu.UseVisualStyleBackColor = true;
+            txtMenu.UseVisualStyleBackColor = false;
             // 
             // panel3
             // 
-            panel3.BackColor = Color.FromArgb(192, 0, 0);
-            panel3.Location = new Point(55, 112);
+            panel3.BackColor = Color.OrangeRed;
+            panel3.Location = new Point(89, 110);
             panel3.Name = "panel3";
-            panel3.Size = new Size(86, 581);
+            panel3.Size = new Size(16, 567);
             panel3.TabIndex = 7;
             // 
             // button3
             // 
+            button3.BackColor = Color.LightGray;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button3.Location = new Point(318, 157);
             button3.Name = "button3";
-            button3.Size = new Size(207, 49);
+            button3.Size = new Size(207, 76);
             button3.TabIndex = 6;
             button3.Text = "Pizza de calabresa";
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
-            button4.Location = new Point(318, 371);
+            button4.BackColor = Color.LightGray;
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(318, 344);
             button4.Name = "button4";
-            button4.Size = new Size(207, 49);
+            button4.Size = new Size(207, 76);
             button4.TabIndex = 7;
             button4.Text = "Pastel de calabresa";
-            button4.UseVisualStyleBackColor = true;
+            button4.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            button2.BackColor = Color.LightGray;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button2.Location = new Point(318, 551);
             button2.Name = "button2";
-            button2.Size = new Size(207, 49);
+            button2.Size = new Size(207, 76);
             button2.TabIndex = 10;
             button2.Text = "Coca 1L";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             // 
             // label4
             // 
@@ -157,64 +174,82 @@
             // 
             // button5
             // 
+            button5.BackColor = Color.LightGray;
+            button5.FlatStyle = FlatStyle.Popup;
+            button5.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button5.Location = new Point(629, 551);
             button5.Name = "button5";
-            button5.Size = new Size(207, 49);
+            button5.Size = new Size(207, 70);
             button5.TabIndex = 12;
             button5.Text = "Sprite 1L";
-            button5.UseVisualStyleBackColor = true;
+            button5.UseVisualStyleBackColor = false;
             // 
             // button6
             // 
-            button6.Location = new Point(629, 371);
+            button6.BackColor = Color.LightGray;
+            button6.FlatStyle = FlatStyle.Popup;
+            button6.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.Location = new Point(629, 344);
             button6.Name = "button6";
-            button6.Size = new Size(207, 49);
+            button6.Size = new Size(207, 76);
             button6.TabIndex = 13;
-            button6.Text = "Pizza de frango c/ catutipy";
-            button6.UseVisualStyleBackColor = true;
+            button6.Text = "Pastel de frango c/ catutipy";
+            button6.UseVisualStyleBackColor = false;
             // 
             // button7
             // 
+            button7.BackColor = Color.LightGray;
+            button7.FlatStyle = FlatStyle.Popup;
+            button7.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button7.Location = new Point(629, 157);
             button7.Name = "button7";
-            button7.Size = new Size(207, 49);
+            button7.Size = new Size(207, 76);
             button7.TabIndex = 14;
             button7.Text = "Pizza de frango c/ catupiry";
-            button7.UseVisualStyleBackColor = true;
+            button7.UseVisualStyleBackColor = false;
             // 
             // button8
             // 
+            button8.BackColor = Color.LightGray;
+            button8.FlatStyle = FlatStyle.Popup;
+            button8.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button8.Location = new Point(923, 157);
             button8.Name = "button8";
-            button8.Size = new Size(207, 49);
+            button8.Size = new Size(207, 76);
             button8.TabIndex = 15;
-            button8.Text = "Pizza de calabresa";
-            button8.UseVisualStyleBackColor = true;
+            button8.Text = "Pizza de queijo ";
+            button8.UseVisualStyleBackColor = false;
             // 
             // button9
             // 
-            button9.Location = new Point(923, 551);
+            button9.BackColor = Color.LightGray;
+            button9.FlatStyle = FlatStyle.Popup;
+            button9.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.Location = new Point(923, 540);
             button9.Name = "button9";
-            button9.Size = new Size(207, 49);
+            button9.Size = new Size(207, 76);
             button9.TabIndex = 16;
-            button9.Text = "Pizza de calabresa";
-            button9.UseVisualStyleBackColor = true;
+            button9.Text = "Pepsi 1L";
+            button9.UseVisualStyleBackColor = false;
             // 
             // button10
             // 
-            button10.Location = new Point(933, 371);
+            button10.BackColor = Color.LightGray;
+            button10.FlatStyle = FlatStyle.Popup;
+            button10.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button10.Location = new Point(933, 344);
             button10.Name = "button10";
-            button10.Size = new Size(207, 49);
+            button10.Size = new Size(207, 76);
             button10.TabIndex = 17;
-            button10.Text = "Pizza de calabresa";
-            button10.UseVisualStyleBackColor = true;
+            button10.Text = "Pastel de queijo";
+            button10.UseVisualStyleBackColor = false;
             // 
             // Encomanda
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.SeaShell;
-            ClientSize = new Size(1444, 722);
+            ClientSize = new Size(1331, 737);
             Controls.Add(button10);
             Controls.Add(button9);
             Controls.Add(button8);
@@ -227,6 +262,7 @@
             Controls.Add(button3);
             Controls.Add(panel1);
             Controls.Add(panel2);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Encomanda";
             Text = "Encomanda";
             panel1.ResumeLayout(false);
